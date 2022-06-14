bd1e6fe87736ca25811ebf01f59e8bb8ea5c4e28
Soumyadeep Ghosh <ghoshsoumyadeep3@gmail.com>
2022-06-14 23:33:07 +0530

    First commit

---
+++ ArrayMethods.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+
+
+public interface IPerson
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Id { get; set; }
+    public int Age { get; set; }
+
+}
+
+public class Employee : IPerson
+{
+    public Employee()
+    {
+        
+    }
+    public Employee(string fName, string lName, int empId=0)
+    {
+        FirstName=fName;
+        LastName=lName;
+        Id= empId;
+    }
+     public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Id { get; set; }
+    public int Age { get; set; }
+
+    //employee properties
+
+    public int EmployeeId { get; set; }
+    public DateOnly StartDate {get; set;}
+    public TimeOnly ShiftStartTime { get; set; }
+
+}
+
+public class Manager: Employee, IPerson
+{
+    public Manager(string fName, string lName, int empId=0): base ( fName,  lName,  empId=0)
+    {
+        
+    }
+    public int NumberOfDirectReports { get; set; }
+}
+
+
---
+++ MainClass.cs
@@ -0,0 +1,8 @@
+using System;
+
+Employee e= new Employee{
+    FirstName="Soumyadeep",
+    LastName="Ghosh",
+    Id=12
+};
+Console.WriteLine("Hello from "+e.FirstName+ " "+e.LastName + " "+ e.Id );
